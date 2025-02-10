2f80b93661b7711080c5b75c7fbbc9b4e291b276
Benny <benjamin.confiac@gmail.com>
2025-02-10 13:51:03 +0900

    friends activity ui

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -22,7 +22,7 @@ const AlbumPage = () => {
   if (isLoading) return null;
   return (
     <div className="h-full">
-      <ScrollArea className="h-full">
+      <ScrollArea className="h-full rounded-md">
         {/* Main content */}
         <div className="relative min-h-full">
           {/* bg gradient */}
