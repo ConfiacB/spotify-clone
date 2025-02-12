7f453d46f40bd56774986c414088389efc0e0cfb
Benny <benjamin.confiac@gmail.com>
2025-02-12 11:00:17 +0900

    socket.io frontend init

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -26,6 +26,15 @@ export interface Stats {
   totalUsers: number;
 }
 
+export interface Message {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface User {
   _id: string;
   clerkId: string;
