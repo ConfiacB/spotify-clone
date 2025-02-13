4c0205a25c476e6da425f09f35be1166c743ff7e
Benny <benjamin.confiac@gmail.com>
2025-02-13 12:10:50 +0900

    vercel deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -68,12 +68,14 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+/*
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+  */
 
 // error handling middleware
 app.use((err, req, res, next) => {
